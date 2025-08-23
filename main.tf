4744a5db3ddb75118d3fce7a7a8e75b858717b12
ved727272 <brijesh.zte@gmail.com>
2025-08-23 14:31:05 +0530

    2rg added

---
+++ main.tf
@@ -0,0 +1,9 @@
+resource "azurerm_resource_group" "rg1" {
+  name     = "vipin-rg"
+  location = "West Europe"
+}
+
+resource "azurerm_resource_group" "rg2" {
+  name     = "vipin-rg2"
+  location = "West Europe"
+}
